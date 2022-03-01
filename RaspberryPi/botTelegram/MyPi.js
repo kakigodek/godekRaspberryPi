c43fa7c13eafd9dcb9f38717881687cd7e362f32
kakigodek <100082356+kakigodek@users.noreply.github.com>
2022-03-01 23:07:10 +0800

    Create MyPi.js

---
+++ RaspberryPi/botTelegram/MyPi.js
@@ -0,0 +1,57 @@
+const {Telegraf} = require('telegraf')
+const {execSync} = require('child_process')
+
+const bot = new Telegraf('Put Token Here')
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+console.log("MyNodeJS has started. Start conversations in your Telegram.");
+
+bot.start((ctx) => ctx.reply('Hello there! I am Kakigodek bot Telegram. Type /help to start'))
+
+bot.command('help', (ctx)=>{
+    console.log(ctx.from)
+    ctx.deleteMessage();
+    ctx.telegram.sendMessage(ctx.chat.id, 'Please select your request :-',
+    {
+        reply_markup:{
+            inline_keyboard: [
+                [{text: "My Raspberry Pi IP Adress", callback_data:"ipScript"}],
+                [{text: "My Raspberry Pi Uptime", callback_data:"uptime"}]
+            ]
+        }
+    })
+})
+
+bot.action('ipScript', (ctx)=>{
+    ctx.deleteMessage();
+
+    const output = execSync('hostname -I', { encoding: 'utf-8' });
+    let oL = output;
+    ctx.telegram.sendMessage(ctx.chat.id, 'Your Raspberry Pi IP is :')
+    ctx.telegram.sendMessage(ctx.chat.id, oL)
+    console.log(ctx.from, oL)
+    sleep(500).then(() => {
+         ctx.telegram.sendMessage(ctx.chat.id, 'Type /help to get menu')
+    });
+
+})
+
+bot.action('uptime', (ctx)=>{
+    ctx.deleteMessage();
+
+    const output = execSync('uptime -p', { encoding: 'utf-8' });
+    let oL = output;
+    ctx.telegram.sendMessage(ctx.chat.id, 'Your Raspberry Pi Uptime is :')
+    ctx.telegram.sendMessage(ctx.chat.id, oL)
+    console.log(ctx.from, oL)
+    sleep(500).then(() => {
+         ctx.telegram.sendMessage(ctx.chat.id, 'Type /help to get menu')
+    });
+
+})
+
+
+bot.launch()
