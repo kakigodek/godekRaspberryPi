7dc09118129c4465789db81404124f08e8590497
kakigodek <100082356+kakigodek@users.noreply.github.com>
2022-03-08 00:08:14 +0800

    Update and rename validateRaspberryPiDevice.sh to PiCopyProtectionSD-Card.sh
    add - timestamp, delay & cron example command.

--- RaspberryPi/misc/PiCopyProtectionSD-Card.sh
+++ RaspberryPi/misc/PiCopyProtectionSD-Card.sh
@@ -3,21 +3,24 @@
 //Youtube : www.youtube.com/c/kakigodek
 //TikTok  : tiktok.com/@kakigodek?lang=en
 //email   : kakig0dek2@gmail.com
-//Desc    : Script to prevent your Raspberry Pi System to be REPLICATE. Start this script at cron.
+//Desc    : Copy protection SD-Card Script, to prevent somebody to copy/duplicate your Raspberry Pi Product. Start this script at cron.
+//Cron cmd: @reboot /path/to/script >> /path/to/log
 //====================================================
 #!/bin/bash
+NOW=$(date)
+sleep 1m ##Pause 1 minutes before start process.
 
 #set original serial number to compare
 #execute this command to get serial number : sudo cat /sys/firmware/devicetree/base/serial-number
-original=10000000b3f2ca6a
+original=Change to your device serial number.
 
 #get Serial number from hardware
 getSerial=$(tr -d '\0' < /sys/firmware/devicetree/base/serial-number)
 
 if [[ ( $original == $getSerial ) ]]; then
-        echo "valid user"
+        echo "$NOW :: valid user"
 else
-        echo "invalid user"
+        echo "$NOW :: invalid user"
         #You can proced next process if not valid (shutdown/shut interface/stop service etc..)
         #sudo shutdown -h now #uncomment this line if you want to shutdown your device.
 fi
